f739e45fed5e2a84633a949fcfa33ab0f1ddc1ab
Michał Szczepański <merapidev@gmail.com>
2017-07-28 22:06:47 +0200

    Nowy adres URL, widok i szablon przeznaczony do wyswietlania listy

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,9 @@ import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -13,12 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as exc:
+                if time.time() - start_time > MAX_WAIT:
+                    raise exc
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edyta dowiedziałą się o nowej, wspaniałej aplikacji w postaci listy rzeczy do zrobienia
@@ -28,7 +36,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Zwrociła uwagę, że tytuł strony i nagłwek zawierający słowo Lisy
         self.assertIn('Listy', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('lista', header_text)
+        self.assertIn('listę', header_text)
 
         # Od razu zostaje zachęconam aby wpisać rzecz do zrobienia
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -43,9 +51,11 @@ class NewVisitorTest(LiveServerTestCase):
         # Po naciśnięciu klawisza Enter strona została zaktualizowana i wyświetla
         # "1: Kupić pawie piora" jako element listy rzeczy do zrobienia
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
-        self.check_for_row_in_list_table(row_text='1: Kupić pawie piora')
+        self.wait_for_row_in_list_table(row_text='1: Kupić pawie piora')
+        
 
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania
         # Edyta wpisała "Użyć pawich pior do zrobienia przynęty"
@@ -55,14 +65,15 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy
         # na liście rzeczy do zrobienia
-        self.check_for_row_in_list_table(row_text='1: Kupić pawie piora')
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(row_text='1: Kupić pawie piora')
+        self.wait_for_row_in_list_table(
             row_text='2: Użyć pawich pior do zrobienia przynęty')
 
         # Teraz nowy użytkownik Franek zaczyna korzystać z witryny
 
-        ## Używa nowej sesji przeglądarki internetowej, aby mieć pewność, że żadne
-        ## informacje dotyczące Edyty nie zostaną ujawnione, na przykłąd przez cookies
+        # Używa nowej sesji przeglądarki internetowej, aby mieć pewność, że żadne
+        # informacje dotyczące Edyty nie zostaną ujawnione, na przykłąd przez
+        # cookies
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
@@ -78,6 +89,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Kupić mleko')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Franek otrzymuje unikatowy adres URL prowadzący do listy
         francis_list_url = self.browser.current_url
@@ -88,7 +100,6 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Kupić pawie piora', page_text)
         self.assertIn('Kupić mleko', page_text)
-        
 
         # Usatysfakcjonowana kładzie się spać
         self.fail('Zakonczenie testu!')
