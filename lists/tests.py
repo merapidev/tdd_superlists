cc2e35711f42ea5734163f6333eedb89802895a5
Michał Szczepański <merapidev@gmail.com>
2017-08-17 23:35:32 +0200

    Usunąłem błąd w widoku prezentującym listę

--- lists/tests.py
+++ lists/tests.py
@@ -56,18 +56,25 @@ class ListAndItemModelTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text="itemey 1", list=list_)
-        Item.objects.create(text="itemey 2", list=list_)
+        correct_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=correct_list)
+        Item.objects.create(text="itemey 2", list=correct_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        other_list = List.objects.create()
+        Item.objects.create(text="Element pierwszy innej listy", list=other_list)
+        Item.objects.create(text="Element drugi innej listy", list=other_list)
+
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'Element pierwszy innej listy')
+        self.assertNotContains(response, 'Element drugi innej listy')
 
 
 class NewListTest(TestCase):
@@ -85,5 +92,8 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post(
             '/lists/new', data={'item_text': 'Nowy element listy'})
+            
+        new_list = List.objects.first()
+        test = '/lists/%d/' % (new_list.id,)
 
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, test)
